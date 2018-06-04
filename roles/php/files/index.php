1782bfb38e95adfd5bafac1132b7d7682068c2b2
Dexter Cardones <dcardones@gmail.com>
2018-06-04 21:40:59 +1000

    Changed php file to reflect Ansible pull

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
     <h1><?php echo " Welcome to DeVops on AWS. This file was generated using php7" ?></h1>
+    <h2><?php echo " This change is done thru Ansible pull" ?></h2>
 </body>
 </html>
