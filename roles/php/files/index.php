4c391e8701e431a65994171d5a42b0dfd9382e47
Dexter Cardones <dcardones@gmail.com>
2018-06-04 21:50:34 +1000

    Correct spelling

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -4,7 +4,7 @@
     <title>Welcome to DevOps on AWS</title>
 </head>
 <body>
-    <h1><?php echo " Welcome to DeVops on AWS. This file was generated using php7" ?></h1>
+    <h1><?php echo " Welcome to DevOps on AWS. This file was generated using php7" ?></h1>
     <h2><?php echo " This change is done thru Ansible pull" ?></h2>
 </body>
 </html>
